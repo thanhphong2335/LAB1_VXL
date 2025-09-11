b5d8091d56c698aa3765eda6d51f4ef39d8c0102
THANHPHONG\Thanh Phong <phong.le2701@hcmut.edu.vn>
2025-09-12 02:35:57 +0700

    init ex3

--- lab1_ex/Core/Inc/main.h
+++ lab1_ex/Core/Inc/main.h
@@ -58,10 +58,30 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_RED_Pin GPIO_PIN_5
-#define LED_RED_GPIO_Port GPIOA
-#define LED_YELLOW_Pin GPIO_PIN_6
-#define LED_YELLOW_GPIO_Port GPIOA
+#define LED_RED2_Pin GPIO_PIN_0
+#define LED_RED2_GPIO_Port GPIOA
+#define LED_YELLOW2_Pin GPIO_PIN_1
+#define LED_YELLOW2_GPIO_Port GPIOA
+#define LED_GREEN2_Pin GPIO_PIN_2
+#define LED_GREEN2_GPIO_Port GPIOA
+#define LED_RED1_Pin GPIO_PIN_5
+#define LED_RED1_GPIO_Port GPIOA
+#define LED_YELLOW1_Pin GPIO_PIN_6
+#define LED_YELLOW1_GPIO_Port GPIOA
+#define LED_GREEN1_Pin GPIO_PIN_7
+#define LED_GREEN1_GPIO_Port GPIOA
+#define LED_RED3_Pin GPIO_PIN_8
+#define LED_RED3_GPIO_Port GPIOA
+#define LED_YELLOW3_Pin GPIO_PIN_9
+#define LED_YELLOW3_GPIO_Port GPIOA
+#define LED_GREEN3_Pin GPIO_PIN_10
+#define LED_GREEN3_GPIO_Port GPIOA
+#define LED_RED4_Pin GPIO_PIN_11
+#define LED_RED4_GPIO_Port GPIOA
+#define LED_YELLOW4_Pin GPIO_PIN_12
+#define LED_YELLOW4_GPIO_Port GPIOA
+#define LED_GREEN4_Pin GPIO_PIN_13
+#define LED_GREEN4_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
